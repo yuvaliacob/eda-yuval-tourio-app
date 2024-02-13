795b60006d19f3e76201867f716185eb398d8ece
edakizak <edakizak.de@gmail.com>
2024-02-13 18:28:19 +0100

    Task: Create From Done

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,15 +4,41 @@ import Place from "../../../db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
 
+  //   if (request.method === "GET") {
+  //     try {
+  //       const places = await Place.find();
+  //       // console.log("Places: ", places);
+  //       response.status(200).json(places);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+
+  //   if (request.method === "POST") {
+  //     try {
+  //       const placeData = request.body;
+  //       await Place.create(placeData);
+  //       response.status(201).json({ status: "Place created" });
+  //     } catch (error) {
+  //       console.log(error);
+  //       response.status(400).json({ error: error.message });
+  //     }
+  //   }
+  // }
+
   if (request.method === "GET") {
-    try {
-      const places = await Place.find();
-
-      console.log("Places: ", places);
-
-      response.status(200).json(places);
-    } catch (error) {
-      console.error(error);
+    const places = await Place.find();
+    // console.log("Places: ", places);
+    response.status(200).json(places);
+  }
+  try {
+    if (request.method === "POST") {
+      const places = request.body;
+      await Place.create(places);
+      response.status(201).json({ status: "Place created" });
     }
+  } catch (error) {
+    console.error(error);
+    response.status(400).json({ status: `Error meseage: ${error.message}` });
   }
 }
